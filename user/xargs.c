02cf932ec15c0712436c49258da12c9585a721b8
alex2023-design <3333ducanh@gmail.com>
2025-03-01 22:14:03 +0700

    Bổ sung

--- user/xargs.c
+++ user/xargs.c
@@ -13,26 +13,26 @@ int main(int argc, char *argv[]) {
 
     char *cmd[MAXARGS];
     for (int i = 1; i < argc; i++) {
-        cmd[i - 1] = argv[i];  // Sao chép lệnh và đối số đầu tiên
+        cmd[i - 1] = argv[i];
     }
 
     char buf[MAXLEN];
     int len = 0;
     
-    while (read(0, &buf[len], 1) == 1) {  // Đọc từ stdin
-        if (buf[len] == '\n') {  // Khi gặp dòng mới
+    while (read(0, &buf[len], 1) == 1) {
+        if (buf[len] == '\n') {
             buf[len] = 0;
-            cmd[argc - 1] = buf;  // Thêm tham số từ stdin
-            cmd[argc] = 0;  // Đảm bảo danh sách tham số kết thúc
+            cmd[argc - 1] = buf;
+            cmd[argc] = 0;
 
             if (fork() == 0) {
-                exec(cmd[0], cmd);  // Thực thi lệnh
+                exec(cmd[0], cmd);
                 fprintf(2, "xargs: exec failed\n");
                 exit(1);
             } else {
                 wait(0);
             }
-            len = 0;  // Reset bộ đệm
+            len = 0;
         } else {
             len++;
         }
